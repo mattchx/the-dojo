5a7e1f86c9eabd90758b24299f63fb6e19672af8
Matt <mattcharlesh@gmail.com>
2021-12-21 11:28:51 -0500

    Users can signup and upload thumbnail img

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { projectAuth } from '../firebase/config'
+import { projectAuth, projectStorage } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
@@ -8,20 +8,27 @@ export const useSignup = () => {
   const [isPending, setIsPending] = useState(false)
   const { dispatch } = useAuthContext()
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null)
     setIsPending(true)
-  
+
     try {
       // signup
-      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      )
 
       if (!res) {
         throw new Error('Could not complete signup')
       }
 
+      // upload user thumbnail
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`
+      const img = await projectStorage.ref(uploadPath).put(thumbnail)
+      const imgUrl = await img.ref.getDownloadURL()
       // add display name to user
-      await res.user.updateProfile({ displayName })
+      await res.user.updateProfile({ displayName, photoURL: imgUrl })
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
@@ -30,8 +37,7 @@ export const useSignup = () => {
         setIsPending(false)
         setError(null)
       }
-    } 
-    catch(err) {
+    } catch (err) {
       if (!isCancelled) {
         setError(err.message)
         setIsPending(false)
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 
 // styles
 import './Signup.css'
@@ -8,21 +9,48 @@ export default function Signup() {
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
   const [thumbnail, setThumbnail] = useState(null)
-  
+  const [thumbnailError, setThumbnailError] = useState(null)
+  const { signup, isPending, error } = useSignup()
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password, displayName)
+    signup(email, password, displayName, thumbnail)
+  }
+
+  const handleFileChange = (e) => {
+    setThumbnail(null)
+    let selected = e.target.files[0]
+    console.log(selected)
+
+    if (!selected) {
+      setThumbnailError('Please select a file')
+      return
+    }
+
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image')
+      return
+    }
+
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb')
+      return
+    }
+
+    setThumbnailError(null)
+    setThumbnail(selected)
+    console.log('thumbnail updated')
   }
 
   return (
-    <form onSubmit={handleSubmit} className="auth-form">
+    <form onSubmit={handleSubmit} className='auth-form'>
       <h2>sign up</h2>
       <label>
         <span>email:</span>
         <input
-          required 
-          type="email" 
-          onChange={(e) => setEmail(e.target.value)} 
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
       </label>
@@ -30,8 +58,8 @@ export default function Signup() {
         <span>password:</span>
         <input
           required
-          type="password" 
-          onChange={(e) => setPassword(e.target.value)} 
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
       </label>
@@ -39,19 +67,24 @@ export default function Signup() {
         <span>display name:</span>
         <input
           required
-          type="text" 
-          onChange={(e) => setDisplayName(e.target.value)} 
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
           value={displayName}
         />
       </label>
       <label>
         <span>Profile thumbnail:</span>
-        <input 
-          required
-          type="file" 
-        />
+        <input required type='file' onChange={handleFileChange} />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
-      <button className="btn">Sign up</button>
+      {!isPending && <button className='btn'>Sign up</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          Loading
+        </button>
+      )}
+
+      {error && <div className='error'>{error}</div>}
     </form>
   )
 }
