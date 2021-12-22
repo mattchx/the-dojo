f2126a108f0509fa2e49c1cd0412243b1edacc1a
Matt <mattcharlesh@gmail.com>
2021-12-22 13:43:49 -0500

    Added avatar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,33 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 
 // styles & images
-import "./Sidebar.css"
+import './Sidebar.css'
 import DashboardIcon from '../assets/dashboard_icon.svg'
 import AddIcon from '../assets/add_icon.svg'
+import Avatar from './Avatar'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Sidebar() {
+  const { user } = useAuthContext()
+  console.log(user)
   return (
-    <div className="sidebar">
-      <div className="sidebar-content">
-        <div className="user">
-          {/* avatar & username here later */}
-          <p>Hey user</p>  
-        </div>  
-        <nav className="links">
+    <div className='sidebar'>
+      <div className='sidebar-content'>
+        <div className='user'>
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
+        </div>
+        <nav className='links'>
           <ul>
             <li>
-              <NavLink exact to="/">
-                <img src={DashboardIcon} alt="dashboard icon" />
+              <NavLink exact to='/'>
+                <img src={DashboardIcon} alt='dashboard icon' />
                 <span>Dashboard</span>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/create">
-                <img src={AddIcon} alt="add project icon" />
+              <NavLink to='/create'>
+                <img src={AddIcon} alt='add project icon' />
                 <span>New Project</span>
               </NavLink>
             </li>
